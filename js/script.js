541b8813ba5ee3fd1efdf3be7120a600a92dbf98
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2022-01-17 16:15:52 +0200

    add contact form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 jQuery(function ($) {
     $(document).ready(function () {
+        // Restricts input for the set of matched elements to the given inputFilter function.
+        $.fn.inputFilter = function(inputFilter) {
+            return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+                if (inputFilter(this.value)) {
+                    this.oldValue = this.value;
+                    this.oldSelectionStart = this.selectionStart;
+                    this.oldSelectionEnd = this.selectionEnd;
+                } else if (this.hasOwnProperty("oldValue")) {
+                    this.value = this.oldValue;
+                    this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+                } else {
+                    this.value = "";
+                }
+            });
+        };
+        
         //add icons and names of uploaded files 
         $('.form-wrap').on('change', '.input-file', function(e){
             let files = this.files;
@@ -15,6 +31,48 @@ jQuery(function ($) {
             filesWrap.html(fileList);
         });
         //validation
+        $('.contact-form').on('change', 'input', function(e){
+            let erroreArrayElemnts = [];
+            let checkbox = $('.contact-form').find('input[type=checkbox][data-reqired]');
+            for (let i = 0; i < checkbox.length; i++) {
+                if(!$(checkbox[i]).is(':checked')){
+                    erroreArrayElemnts.push(checkbox[i]);
+                }
+            }
+            let inputs = $('.contact-form').find('input[type=text][data-reqired]');
+            for (let i = 0; i < inputs.length; i++) {
+                if(!$(inputs[i]).val()){
+                    erroreArrayElemnts.push(inputs[i]);
+                }
+            }
+            let phone = $('.contact-form').find('.phone-input');
+            if(phone.length > 0) {
+                if(phone.val()) {
+                    if(!validatePhone(phone.val())){
+                        erroreArrayElemnts.push(phone);
+                    }
+                } else {
+                    erroreArrayElemnts.push(phone);
+                }
+            }
+            let email = $('.contact-form').find('.email-input');
+            if(email.length > 0) {
+                if(email.val()) {
+                    if(!isEmail(email.val())){
+                        erroreArrayElemnts.push(email);
+                    }
+                } else {
+                    erroreArrayElemnts.push(email);
+                }
+            }
+            console.log(erroreArrayElemnts);
+            if(erroreArrayElemnts.length === 0){
+                $('.form-valid').find('.btn-submit').removeClass('inactive');
+            } else {
+                $('.form-valid').find('.btn-submit').addClass('inactive');
+            }
+        });
+
         var formValid = document.getElementsByClassName('form-valid')[0];
         $('.form-valid').on('click', '.btn-submit', function(e) {
             $(this).parents('form').submit(function (e) {
@@ -54,7 +112,7 @@ jQuery(function ($) {
             });
         });
 
-        $('form').on('change', '.email-input', function(e){
+        $('form').on('change focusout', '.email-input', function(e){
             if($(this).val()){
                 if(!isEmail($(this).val())){
                     $(this).parents('.form-group').find('.error-text').html('Ввведите email в верном формате');
@@ -65,10 +123,17 @@ jQuery(function ($) {
                         $(this).parents('.form-group').find('.error-text').fadeOut(300);
                     });
                 }
+            } else {
+                $(this).parents('.form-group').addClass('has-error');
+                $(this).parents('.form-group').find('.error-text').fadeIn(300);
+                $(this).focus(function(e){
+                    $(this).parents('.form-group').removeClass('has-error');
+                    $(this).parents('.form-group').find('.error-text').fadeOut(300);
+                });
             }
         });
 
-        $('form').on('change', '.phone-input', function(e){
+        $('form').on('change focusout', '.phone-input', function(e){
             if($(this).val()){
                 if(!validatePhone($(this).val())){
                     $(this).parents('.form-group').find('.error-text').html('Ввведите номер телефона в верном формате');
@@ -78,8 +143,28 @@ jQuery(function ($) {
                         $(this).parents('.form-group').removeClass('has-error');
                         $(this).parents('.form-group').find('.error-text').fadeOut(300);
                     });
-                } 
+                }
+            } else {
+                $(this).parents('.form-group').addClass('has-error');
+                $(this).parents('.form-group').find('.error-text').fadeIn(300);
+                $(this).focus(function(e){
+                    $(this).parents('.form-group').removeClass('has-error');
+                    $(this).parents('.form-group').find('.error-text').fadeOut(300);
+                });   
             }
+        });
+
+        
+        $('form').on('change focusout', '.name-input', function(e){
+            if(!$(this).val()){
+                $(this).parents('.form-group').find('.error-text').html('Ввведите имя');
+                $(this).parents('.form-group').addClass('has-error');
+                $(this).parents('.form-group').find('.error-text').fadeIn(300);
+                $(this).focus(function(e){
+                    $(this).parents('.form-group').removeClass('has-error');
+                    $(this).parents('.form-group').find('.error-text').fadeOut(300);
+                });
+        }
         });
 
         function isEmail(email) {
@@ -96,5 +181,9 @@ jQuery(function ($) {
             }
         }
 
+        //only number for phone
+        $('.phone-input').inputFilter(function(value) {
+            return /^-?\+?\d*$/.test(value) && (value === "" || value);
+        });
     });
 });
